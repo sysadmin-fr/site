cc9d790b6ea21fc4d8e62210cfcc9fd431b43bc1
Bruno Bonfils <asyd@asyd.net>
2012-01-15 02:57:43 +0100

    Ajout de l'image pour les pre Modification de certains articles

--- lib/helpers/navigation.rb
+++ lib/helpers/navigation.rb
@@ -1,17 +1,15 @@
 module Navigation
-  def start_navigation
-    "<div class='navigation'>"
-  end
   
-  def stop_navigation
-    "<div class='clear-float'></div></div>"
-  end
-  
-  def previous_section(title, link)
-    "<div class='previous-link'><a href=#{link}>← #{title}</a></div>"
+  def navigation(data)
+    buffer = "<div class='navigation'>"
+    if data.has_key?(:previous_title)
+      buffer += "<div class='previous-link'><a href=#{data[:previous_link]}>← #{data[:previous_title]}</a></div>"
+    end
+    if data.has_key?(:next_title)
+      buffer += "<div class='next-link'><a href=#{data[:next_link]}>#{data[:next_title]} →</a></div>"
+    end
+    buffer += "</div><div class='clear-float'></div>"
+    buffer
   end
 
-  def next_section(title, link)
-    "<div class='next-link'><a href=#{link}>#{title} →</a></div>"
-  end
 end
